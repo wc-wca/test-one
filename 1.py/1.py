4a4cc98235ec2527889ddf792da3c6b61665710e
wc-wca <2598373356@qq.com>
2025-09-03 15:26:51 +0800

    更新

--- 1.py/1.py
+++ 1.py/1.py
@@ -1,1 +1,8 @@
-11
+'''
+Author: wc-wca 2598373356@qq.com
+Date: 2025-09-03 15:13:00
+LastEditors: wc-wca 2598373356@qq.com
+LastEditTime: 2025-09-03 15:26:26
+FilePath: \1\1.py\1.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
